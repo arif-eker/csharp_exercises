d251445295853bdbc0cd1713f4a23665a64b6cbb
Arif Eker <59051618+arif-eker@users.noreply.github.com>
2021-02-20 17:32:03 +0300

    Add files via upload

---
+++ Events/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void StockControl();
+
+    public class Product
+    {
+        private int _stock;
+
+        public Product(int stock
+        )
+        {
+            _stock = stock;
+        }
+
+        public event StockControl StockKontrolEvent;
+        public string ProductName { get; set; }
+
+        public int Stock
+        {
+            get { return _stock; }
+            set
+            {
+                _stock = value;
+                // kullanıcı bu evente kaydolmuş mu ?
+                if (value<=15&&StockKontrolEvent!= null)
+                {
+                    StockKontrolEvent();
+                }
+            }
+        }
+
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            Console.WriteLine("{1} Stock amount {0}", Stock, ProductName);
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product harddisk = new Product(50);
+
+            harddisk.ProductName = "Hard Disk";
+            Product telefon = new Product(50);
+            telefon.ProductName = "Telefon";
+
+            //event'e kaydoluyoruz.
+            telefon.StockKontrolEvent += Telefon_StockKontrolEvent;
+
+            for (int i = 0; i < 10; i++)
+            {
+                harddisk.Sell(10);
+                telefon.Sell(10);
+                Console.ReadKey();
+            }
+
+
+            Console.ReadKey();
+        }
+
+        private static void Telefon_StockKontrolEvent()
+        {
+            Console.WriteLine("Telefon 15'ten az kaldı, bitiyor!!!");
+        }
+    }
+}
